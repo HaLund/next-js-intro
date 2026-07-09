e6ad1dbe067cf0351954e89152834d7a00fe5728
Hannes Lundin <hanneslundin@hotmail.com>
2026-07-09 16:05:54 +0200

    testing: splitting a list in two

--- app/chemists/page.tsx
+++ app/chemists/page.tsx
@@ -2,7 +2,23 @@ import { people } from '@/utils/images/data/page.js';
 import { getImageUrl } from '@/utils/images/page.js';
 
 export default function List() {
-  const listItems = people.map(person =>
+  const chemists = people.filter(m => m.profession === 'chemist');
+  const nonChemists = people.filter(m => m.profession != 'chemist');
+  const listItems = chemists.map(person => 
+    <li key={person.id}>
+      <img
+        src={getImageUrl(person)}
+        alt={person.name}
+      />
+      <p>
+        <b>{person.name}:</b>
+        {' ' + person.profession + ' '}
+        known for {person.accomplishment}
+      </p>
+    </li>
+  );
+
+  const listItems2 = nonChemists.map(person => 
     <li key={person.id}>
       <img
         src={getImageUrl(person)}
@@ -17,8 +33,10 @@ export default function List() {
   );
   return (
     <article>
-      <h1>Scientists</h1>
+      <h1>Scientists:</h1>
       <ul>{listItems}</ul>
+        <h1>Non-Chemists</h1>
+        <ul>{listItems2}</ul>
     </article>
   );
 }
